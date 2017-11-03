c9595deeab35954b152fa885874ec7c63eb51bba
Diego Occhi <diegocchi@gmail.com>
2017-11-03 10:03:53 -0200

    metodos js

--- www/BluetoothPrinter.js
+++ www/BluetoothPrinter.js
@@ -1,25 +1,23 @@
 var exec = require('cordova/exec');
 
-exports.BTPrinter = {
-   list: function(fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "list", []);
-   },
-   connect: function(name, fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "connect", [name]);
-   },
-   disconnect: function(fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "disconnect", []);
-   },
-   print: function(str, fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
-   },
-   printText: function(str, fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
-   },
-    printImage: function(str, fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printImage", [str]);
-    },
-   printPOSCommand: function(str, fnSuccess, fnError){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printPOSCommand", [str]);
-   }
+exports.list = function (fnSuccess, fnError) {
+  exec(fnSuccess, fnError, "BluetoothPrinter", "list", []);
 };
+exports.connect = function (name, fnSuccess, fnError) {
+  exec(fnSuccess, fnError, "BluetoothPrinter", "connect", [name]);
+};
+exports.disconnect = function (fnSuccess, fnError) {
+  exec(fnSuccess, fnError, "BluetoothPrinter", "disconnect", []);
+};
+exports.print = function (str, fnSuccess, fnError) {
+  exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
+};
+exports.printText = function (str, fnSuccess, fnError) {
+  exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
+};
+exports.printImage = function (str, fnSuccess, fnError) {
+  exec(fnSuccess, fnError, "BluetoothPrinter", "printImage", [str]);
+};
+exports.printPOSCommand = function (str, fnSuccess, fnError) {
+  exec(fnSuccess, fnError, "BluetoothPrinter", "printPOSCommand", [str]);
+}
